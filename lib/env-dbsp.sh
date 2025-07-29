6e11d7d81ad083bacac7ccee3aa85db2d155c34a
Jiyuan Zhang <90346231+jiyuzh@users.noreply.github.com>
2025-07-29 07:36:26 +0000

    feat: tested debootstrap wrapper

--- lib/env-dbsp.sh
+++ lib/env-dbsp.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env false
 
-DEBOOTSTRAP_GIT="https://salsa.debian.org/installer-team/debootstrap.git"
+DEBOOTSTRAP_GIT="${DEBOOTSTRAP_GIT:-"https://salsa.debian.org/installer-team/debootstrap.git"}"
 
+# Check if the debootstrap installation is valid
 function check_debootstrap
 {
 	local dir="$1"
 
 	if [[ -f "$dir/debootstrap" ]] && [[ -x "$dir/debootstrap" ]]; then
-		if "$dir/debootstrap" --help | grep -q "Bootstrap a Debian base system into a target directory."; then
+
+		# use true here as grep -q will terminate on first match and leave a broken pipe
+		if ("$dir/debootstrap" --help || builtin true) | grep -q "Bootstrap a Debian base system into a target directory."; then
 			return 0
 		fi
 	fi
@@ -15,30 +18,36 @@ function check_debootstrap
 	return 1
 }
 
+# Obtain the latest debootstrap from a predefined git repo
 function get_debootstrap
 {
 	local dir="$1"
 
 	# path exists
 	if [[ -e "$dir" ]]; then
-		if check_debootstrap; then
+
+		# check for validity first
+		if check_debootstrap "$dir"; then
 			return 0
-		else
+
+		# let go empty folder and alert all others
+		elif [[ ! -d "$dir" ]] || [[ -n "$(ls -A "$dir")" ]]; then
 			builtin echo "Cannot obtain latest debootstrap: Path '$dir' is occupied"
 			exit 1
 		fi
-	else
-		mkdir -p "$(dirname "$dir")"
+	fi
 
-		git clone --depth 1 "$DEBOOTSTRAP_GIT" "$dir"
+	mkdir -p "$(dirname "$dir")"
 
-		if ! check_debootstrap; then
-			builtin echo "Validation of debootstrap failed: Git '$DEBOOTSTRAP_GIT' cannot provide the right binary"
-			exit 1
-		fi
+	git clone --depth 1 "$DEBOOTSTRAP_GIT" "$dir"
+
+	if ! check_debootstrap "$dir"; then
+		builtin echo "Validation of debootstrap failed: Git '$DEBOOTSTRAP_GIT' cannot provide the right binary"
+		exit 1
 	fi
 }
 
+# Wrapper function for executing debootstrap
 function run_debootstrap
 {
 	local dir="$1"
@@ -46,6 +55,7 @@ function run_debootstrap
 
 	get_debootstrap "$dir"
 
-	# arch shall be explicitly defined, 
-	sudo env DEBOOTSTRAP_DIR="$dir" "$dir/debootstrap" --arch=$(dpkg --print-architecture) "$@"
+	# --arch shall be explicitly defined for non-deb systems, as per Fedora docunment
+	# --keep-debootstrap-dir is semi-undocumented (in changelog only), it allows us to work in a non-empty folder
+	sudo env DEBOOTSTRAP_DIR="$dir" "$dir/debootstrap" --arch=$(dpkg --print-architecture) --keep-debootstrap-dir "$@"
 }
